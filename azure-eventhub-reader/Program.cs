f74ab24d3011ea767e3af470983910a1ace9a1eb
juan <jsburckhardt@gmail.com>
2017-08-16 23:50:21 +1000

    first trial

--- azure-eventhub-reader/Program.cs
+++ azure-eventhub-reader/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace azure_eventhub_reader
     {
         static void Main(string[] args)
         {
+            var client = EventHubClient.CreateFromConnectionString("from https://portal.azure.com/#resource/subscriptions/485bc17f-97bd-40fa-a1b0-918fc639be40/resourceGroups/l22-apidebugging-test-rg/providers/Microsoft.EventHub/namespaces/l22-apidebugging-test/saskey", "iflogssit2");
+            EventHubConsumerGroup group = client.GetDefaultConsumerGroup();
+            var startDate = new DateTime(2017,8,15,0,0,0);
+            var receiver = group.CreateReceiver(client.GetRuntimeInformation().PartitionIds[0],startDate);
+            //var message = receiver.Receive();
+            Console.ReadLine();
+
+            bool receive = true;
+            int counter = 0;
+            while (receive)
+            {
+                var message = receiver.Receive();
+                var body = Encoding.UTF8.GetString(message.GetBytes());
+                var time = message.EnqueuedTimeUtc.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss \"GMT\"zzz");
+
+                
+                Console.WriteLine("MESSAGE " + message.Offset + " @ " + time);
+                Console.WriteLine(body);
+                Console.WriteLine("");
+
+                Console.Write(".");
+                if (counter % 60 == 59)
+                {
+                    Console.WriteLine("");
+                }
+                counter++;
+            }
         }
     }
 }
